7ec9efc852dbecfb40fa854ad4a2be8d2fe25b30
vagnerseibert <vagnerseibert@gmail.com>
2016-10-25 00:49:27 -0200

    Created day-night cycle scheme

---
+++ Zombarbs/Assets/Scripts/DayNightCycleController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCycleController : MonoBehaviour {
+    public float dayLenght = 10;
+    public float nightIntensity = 0f;
+    public float dayIntensity = 1f;
+    Light sun;
+
+    void Start() {
+        sun = GetComponent<Light>();
+    }
+
+    public void Update() {
+        var t = Mathf.Repeat(Time.time / dayLenght, 1);
+
+        if(t < .25f) {
+            sun.intensity = dayIntensity;
+        }
+        else if(t < .5f) {
+            sun.intensity = nightIntensity + dayIntensity - ((t - .25f) / .25f) * (dayIntensity - nightIntensity);
+        }
+        else if(t < .75f) {
+            sun.intensity = nightIntensity;
+        } else {
+            sun.intensity = nightIntensity + ((t - .75f) / .25f) * (dayIntensity - nightIntensity);
+        }
+    }
+}
