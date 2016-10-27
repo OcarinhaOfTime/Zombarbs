b04c3778268b053d75ea66cac0a5872657f455fe
vagnerseibert <vagnerseibert@gmail.com>
2016-10-27 11:55:00 -0200

    Added pyroFX

---
+++ Zombarbs/Assets/Scripts/PutPSToTheFront.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PutPSToTheFront : MonoBehaviour {
+    public string sortingLayerName = "Foreground";
+	void Awake() {
+
+		//SpriteRenderer rend = GetComponent<SpriteRenderer>();
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+		foreach(Renderer r in renderers)
+			r.sortingLayerName = sortingLayerName;
+
+		GetComponent<Renderer>().sortingLayerName = sortingLayerName;
+	}
+}
