e4305309db63305ac0cf471b7755cba235ef7b9b
vagnerseibert <vagnerseibert@gmail.com>
2016-11-17 09:58:08 -0200

    Improvements on animation

--- Zombarbs/Assets/Scripts/Lantern.cs
+++ Zombarbs/Assets/Scripts/Lantern.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Lantern : MonoBehaviour {
     void Update() {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Quaternion rot = Quaternion.LookRotation(transform.position - mousePos, Vector3.forward);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePos, Mathf.Sign(transform.lossyScale.x) * Vector3.forward);
 
         transform.rotation = rot;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + 90);
